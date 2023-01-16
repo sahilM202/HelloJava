7c1279154fb8945f41984d118748f790e3aaa4c9
sahilM202 <sahilmehdizade2022@gmail.com>
2023-01-16 18:08:29 +0300

    hello java file deyisdim

--- hellojava.java
+++ hellojava.java
@@ -2,5 +2,10 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         // #
+
+
+        // &&
+
+        // bu menem sahil
     }
 }
